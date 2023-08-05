887555a55bff774327592343f21b56f2000119d4
kamrowski <lukasz@kamrowsky.pl>
2023-08-05 14:21:49 +0200

    added utils

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,11 +1,1 @@
-import React from "react"
-
-const Header = ({ text = "" }) => {
-    return (
-        <h1>Shared header library {text}</h1>
-    )
-}
-
-export {
-    Header
-}
+export { Header } from './Header';
